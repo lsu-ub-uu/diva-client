999ec775257ffd318dace21e6c3d0591db2e2cfb
danknowit <daniel.flores@knowit.se>
2024-12-23 14:13:51 +0100

    WIP the test works but not on chrome for some reason

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -31,3 +31,73 @@ test('header exists and contains expected elements', async ({ page }) => {
   const loginButton = page.getByRole('button', { name: 'Logga in' });
   await expect(loginButton).toBeVisible();
 });
+
+test('clicking "Logga in" button shows account list', async ({ page }) => {
+
+  await page.goto('http://localhost:5173/');
+
+  const loginButton = page.getByRole('button', { name: 'Logga in' });
+  await loginButton.click();
+
+  await page.waitForSelector('.MuiMenuItem-root');
+
+  const menuItems = await page.locator('.MuiMenuItem-root').all();
+
+  const listItems = await Promise.all(
+    menuItems.map(async (item) => await item.textContent())
+  );
+
+  expect(listItems).toEqual([
+    'Utvecklarkonto',
+    'DiVA Admin',
+    'DiVA Everything',
+    'Admin System',
+    'UU domainAdmin',
+    'KTH domainAdmin',
+    'Gemensam inloggning',
+    'Uppsala universitet (Preview)',
+    'Användarnamn & Lösenord',
+    'Diva Uppsala Universitet (Lösenord)'
+  ]);
+});
+
+test('clicking "Logga in" button shows account list and DiVA Admin works', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+
+  const loginButton = page.getByRole('button', { name: 'Logga in' });
+  await loginButton.click();
+
+  await page.waitForSelector('.MuiMenuItem-root');
+
+  const menuItems = await page.locator('.MuiMenuItem-root').all();
+
+  const listItems = await Promise.all(
+    menuItems.map(async (item) => await item.textContent())
+  );
+
+  // Check if the list contains the expected items
+  expect(listItems).toEqual([
+    'Utvecklarkonto',
+    'DiVA Admin',
+    'DiVA Everything',
+    'Admin System',
+    'UU domainAdmin',
+    'KTH domainAdmin',
+    'Gemensam inloggning',
+    'Uppsala universitet (Preview)',
+    'Användarnamn & Lösenord',
+    'Diva Uppsala Universitet (Lösenord)'
+  ]);
+
+  await page.click('text=DiVA Admin');
+
+  await page.waitForSelector('div.MuiStack-root div.MuiBox-root:has-text("DiVA Admin")');
+
+  const divaAdminText = await page.textContent('div.MuiStack-root div.MuiBox-root:has-text("DiVA Admin")');
+  expect(divaAdminText).toBe('DiVA Admin');
+
+  const logoutButton = await page.textContent('button:has-text("Logga ut")');
+  expect(logoutButton).toBe('Logga ut');
+});
+
+
