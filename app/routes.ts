38b5ccc47f9506ced9f65e737e53837da24b1ea2
Leo Danielsson <leo.danielsson@ub.uu.se>
2025-01-20 16:35:10 +0100

    implement remix future flag v3_routeConfig

---
+++ app/routes.ts
@@ -0,0 +1,24 @@
+import { index, route, type RouteConfig } from '@remix-run/route-config';
+
+export default [
+  index('routes/home.tsx'),
+
+  /* Auth */
+  route('login', 'routes/login.tsx'),
+  route('logout', 'routes/logout.tsx'),
+
+  /* Record CRUD */
+  route('create', 'routes/recordCreate.tsx'),
+  route('view/:recordType/:recordId', 'routes/recordView.tsx'),
+  route('update/:recordType/:recordId', 'routes/recordUpdate.tsx'),
+  route('delete/:recordType/:recordId.tsx', 'routes/recordDelete.tsx'),
+
+  /* Search */
+  route('search/:searchType', 'routes/search.tsx'),
+
+  /* Resource routes */
+  route('record/:recordType/:recordId.tsx', 'routes/getRecord.tsx'),
+  route('refreshDefinitions', 'routes/refreshDefinitions.tsx'),
+  route('autocompleteSearch', 'routes/autocompleteSearch.tsx'),
+  route('translations/:lang', 'routes/translations.tsx'),
+] satisfies RouteConfig;
