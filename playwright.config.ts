dffa337e00a52db1be8ac6598f0735f2cb79b889
Leo Danielsson <leo.danielsson@ub.uu.se>
2025-03-06 08:56:59 +0100

    clean up playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,28 +18,17 @@ const {
  */
 export default defineConfig({
   testDir: './e2e',
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: E2E_TARGET ?? `http://${DOMAIN}:${PORT}${BASE_PATH}`,
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-
     screenshot: 'only-on-failure',
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'firefox',
@@ -63,24 +52,13 @@ export default defineConfig({
     //   name: 'Mobile Safari',
     //   use: { ...devices['iPhone 12'] },
     // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
-  /* Run your local dev server before starting the tests */
   webServer: process.env.E2E_TARGET
     ? undefined
     : {
         command: 'npm run dev',
         url: `http://${DOMAIN}:${PORT}${BASE_PATH}`,
-        reuseExistingServer: true,
+        reuseExistingServer: !process.env.CI,
       },
 });
