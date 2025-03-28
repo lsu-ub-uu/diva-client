2c086fb73106d383d557ae965c4ca627e8ced6a5
egiiiiil <sixteenth.bird@gmail.com>
2025-03-28 16:49:41 +0100

    CORA-2873 Added tests for Accordion

---
+++ app/components/Accordion/__tests__/Accordion.test.tsx
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2025 Uppsala University Library
+ *
+ * This file is part of DiVA Client.
+ *
+ *     DiVA Client is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     DiVA Client is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ */
+
+import { render } from '@testing-library/react';
+import { faker } from '@faker-js/faker';
+import { Accordion } from '@/components/Accordion/Accordion';
+import { AccordionTitle } from '@/components/Accordion/AccordionTitle';
+import { AccordionContent } from '@/components/Accordion/AccordionContent';
+import { AccordionExpandButton } from '@/components/Accordion/AccordionExpandButton';
+
+describe('Accordion', () => {
+  it('accordion with a title', () => {
+    render(
+      <Accordion expanded={accordionExpanded} onChange={setAccordionExpanded}>
+        <AccordionTitle>Accordion title</AccordionTitle>
+        {accordionExpanded && (
+          <AccordionContent>
+            <p>{faker.lorem.paragraph()}</p>
+            <p>{faker.lorem.paragraph()}</p>
+            <p>{faker.lorem.paragraph()}</p>
+          </AccordionContent>
+        )}
+      </Accordion>,
+    );
+  });
+
+  it('Accordion with title and collapsed content', () => {
+    render(
+      <Accordion expanded={accordionExpanded} onChange={setAccordionExpanded}>
+        <AccordionTitle>Accordion title</AccordionTitle>
+        <AccordionContent>
+          {accordionExpanded ? (
+            <>
+              <p>{faker.lorem.paragraph()}</p>
+              <p>{faker.lorem.paragraph()}</p>
+              <p>{faker.lorem.paragraph()}</p>
+            </>
+          ) : (
+            <p>{faker.lorem.paragraph()}</p>
+          )}
+        </AccordionContent>
+      </Accordion>,
+    );
+  });
+
+  it('Accordion without title', () => {
+    render(
+      <Accordion expanded={accordionExpanded} onChange={setAccordionExpanded}>
+        <AccordionContent>
+          {accordionExpanded ? (
+            <>
+              <p>{faker.lorem.paragraph()}</p>
+              <p>{faker.lorem.paragraph()}</p>
+              <p>{faker.lorem.paragraph()}</p>
+            </>
+          ) : (
+            <p>{faker.lorem.paragraph()}</p>
+          )}
+        </AccordionContent>
+        <AccordionExpandButton />
+      </Accordion>,
+    );
+  });
+});
