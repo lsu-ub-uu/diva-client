c23fc8e8d0a77bb61e208a296055f9e3de335615
Leo Danielsson <leo.danielsson@ub.uu.se>
2025-03-31 11:52:26 +0200

    remove svg titles

--- app/components/Alert/__tests__/Alert.test.tsx
+++ app/components/Alert/__tests__/Alert.test.tsx
@@ -1,7 +1,6 @@
 import { describe, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import { Alert, type AlertProps, AlertTitle } from '@/components/Alert/Alert';
-import { SentimentNeutralIcon } from '@/icons';
+import { Alert, AlertTitle } from '@/components/Alert/Alert';
 
 describe('<Alert />', () => {
   it('renders an alert with text', () => {
@@ -9,29 +8,7 @@ describe('<Alert />', () => {
     const alertText = screen.getByText('some alert text');
     expect(alertText).toBeInTheDocument();
   });
-  it.each([
-    ['success', 'CheckCircle'],
-    ['info', 'Info'],
-    ['warning', 'Warning'],
-    ['error', 'Error'],
-  ])('render an alert with icon for %s and text', (arg1, arg2) => {
-    render(
-      <Alert severity={arg1 as AlertProps['severity']}>some alert text</Alert>,
-    );
-    const expected = screen.getByTitle(arg2);
-    expect(expected).toBeInTheDocument();
-    const alertText = screen.getByText('some alert text');
-    expect(alertText).toBeInTheDocument();
-  });
-  it('renders an alert with overridden icon', () => {
-    render(
-      <Alert severity='success' icon={<SentimentNeutralIcon />}>
-        some alert text
-      </Alert>,
-    );
-    const expected = screen.getByTitle('SentimentNeutral');
-    expect(expected).toBeInTheDocument();
-  });
+
   it('render an alert with text and icon, and title', () => {
     render(
       <Alert severity='success'>
