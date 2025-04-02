e04dc448de183c3d9a48c4afbc80ad9b3c10d6aa
egiiiiil <sixteenth.bird@gmail.com>
2025-04-02 15:52:40 +0200

    CORA-2860 Added e2e tests for error pages

---
+++ e2e/errorMissingRecord.test.ts
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2025 Uppsala University Library
+ *
+ * This file is part of DiVA Client.
+ *
+ *     DiVA Client is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     DiVA Client is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ */
+
+import { test } from './util/fixtures';
+import { getFirstDataAtomicValueWithNameInData } from '@/cora/cora-data/CoraDataUtilsWrappers.server';
+import { getFirstDataGroupWithNameInData } from '@/cora/cora-data/CoraDataUtils.server';
+import { expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
+import { createUrl } from './util/createUrl';
+
+test('updates an existing report', async ({ page, divaOutput }) => {
+  const recordId = getFirstDataAtomicValueWithNameInData(
+    getFirstDataGroupWithNameInData(divaOutput, 'recordInfo'),
+    'id',
+  );
+  const recordTitle = getFirstDataAtomicValueWithNameInData(
+    getFirstDataGroupWithNameInData(divaOutput, 'titleInfo'),
+    'title',
+  );
+
+  await page.goto(createUrl(`/diva-output/${recordId}/update`));
+
+  // Log in
+  await page.getByRole('button', { name: 'Logga in' }).click();
+  await page.getByRole('menuitem', { name: 'DiVA Admin' }).click();
+  await expect(page.getByRole('button', { name: 'Logga ut' })).toBeVisible();
+
+  //Assert update page info
+  await expect(page.getByRole('heading', { level: 1 })).toHaveText(recordTitle);
+  await expect(
+    page.getByRole('group', { name: 'Huvudtitel' }).getByLabel('Huvudtitel'),
+  ).toHaveValue(recordTitle);
+});
---
+++ e2e/errorWrongRecordType.test.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2025 Uppsala University Library
+ *
+ * This file is part of DiVA Client.
+ *
+ *     DiVA Client is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     DiVA Client is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ */
+
+import { test } from './util/fixtures';
+import { expect, type Page } from '@playwright/test';
+import { faker } from '@faker-js/faker';
+import { createUrl } from './util/createUrl';
+
+test('Error recordType', async ({ page, request, authtoken }) => {
+  // Go to start page
+  await page.goto(createUrl('/'));
+
+  // Log in
+  await page.getByRole('button', { name: 'Logga in' }).click();
+  await page.getByRole('menuitem', { name: 'DiVA Admin' }).click();
+  await expect(page.getByRole('button', { name: 'Logga ut' })).toBeVisible();
+
+  await page.goto(createUrl('/wrong-diva-output'));
+});
