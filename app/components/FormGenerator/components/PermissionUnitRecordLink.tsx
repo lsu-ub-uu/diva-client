da245c2f5e743c41218a73d94eebf4730e249c6b
Leo Danielsson <leo.danielsson@ub.uu.se>
2025-04-08 14:46:19 +0200

    add mitssing file

---
+++ app/components/FormGenerator/components/PermissionUnitRecordLink.tsx
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2025 Uppsala University Library
+ *
+ * This file is part of DiVA Client.
+ *
+ *     DiVA Client is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     DiVA Client is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ */
+
+import type { FormComponentRecordLink } from '@/components/FormGenerator/types';
+import { useRouteLoaderData } from 'react-router';
+import type { loader } from '@/root';
+import { useEffect } from 'react';
+import { useRemixFormContext } from 'remix-hook-form';
+
+interface PermissionUnitRecordLinkProps {
+  component: FormComponentRecordLink;
+  path: string;
+}
+export const PermissionUnitRecordLink = ({
+  component,
+  path,
+}: PermissionUnitRecordLinkProps) => {
+  const { setValue } = useRemixFormContext();
+  const rootLoaderData = useRouteLoaderData<typeof loader>('root');
+  const memberPermissionUnit = rootLoaderData?.theme?.memberPermissionUnit;
+
+  // If theme.permissionUnit
+  useEffect(() => {
+    if (memberPermissionUnit) {
+      setValue(path, memberPermissionUnit);
+    }
+  }, [setValue, path, memberPermissionUnit]);
+
+  if (memberPermissionUnit) {
+    return null;
+  }
+
+  // If user has 1 permission unit, set it.
+
+  // If user has multiple PU, render input <RecordLinkWithSearch />
+};
