6c5a9b1fdedb8f37db8d60c2ff774b273cc30812
Leo Danielsson <leo.danielsson@ub.uu.se>
2025-04-09 15:29:29 +0200

    Toggle dev mode with query param

--- app/utils/useIsDevMode.ts
+++ app/utils/useIsDevMode.ts
@@ -1,11 +1,23 @@
 import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router';
 
 export const useIsDevMode = () => {
   const [dev, setDev] = useState(false);
+  const [searchParams] = useSearchParams();
+  const searchParam = searchParams.get('dev');
 
   useEffect(() => {
-    const isDev = localStorage.getItem('diva-dev');
-    setDev(isDev !== null);
-  }, []);
+    if (searchParam === 'false') {
+      setDev(false);
+      localStorage.removeItem('diva-dev');
+    } else if (searchParam === 'true') {
+      setDev(true);
+      localStorage.setItem('diva-dev', 'true');
+    } else {
+      const isDev = localStorage.getItem('diva-dev');
+      setDev(isDev !== null);
+    }
+  }, [searchParam]);
+
   return dev;
 };
