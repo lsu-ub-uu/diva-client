0c39e5d7596c3538908a346563edfa3138650752
Leo Danielsson <leo.danielsson@ub.uu.se>
2025-08-04 14:18:49 +0200

    Use floating ui directly for popover instead of HUI Popover

--- app/components/FieldInfo/FieldInfo.tsx
+++ app/components/FieldInfo/FieldInfo.tsx
@@ -16,18 +16,14 @@
  *     You should have received a copy of the GNU General Public License
  */
 
-import {
-  CloseButton,
-  Popover,
-  PopoverButton,
-  PopoverPanel,
-} from '@headlessui/react';
 import { Button } from '@/components/Button/Button';
+import { FieldContext } from '@/components/Input/Fieldset';
 import { CloseIcon, InfoIcon } from '@/icons';
+import { usePopover } from '@/utils/usePopover';
+import { FloatingFocusManager, FloatingPortal } from '@floating-ui/react';
+import { use } from 'react';
 import { useTranslation } from 'react-i18next';
 import styles from './FieldInfo.module.css';
-import { FieldContext } from '@/components/Input/Fieldset';
-import { use } from 'react';
 
 interface FieldInfoProps {
   title: string;
@@ -37,36 +33,56 @@ interface FieldInfoProps {
 export const FieldInfo = ({ title, body }: FieldInfoProps) => {
   const { t } = useTranslation();
   const { ids } = use(FieldContext);
+
+  const {
+    isOpen,
+    setIsOpen,
+    refs,
+    floatingStyles,
+    context,
+    getReferenceProps,
+    getFloatingProps,
+  } = usePopover();
+
   return (
-    <Popover className={styles['popover']}>
-      <PopoverButton
-        as={Button}
+    <div className={styles['popover']}>
+      <Button
         variant='icon'
         size='small'
         aria-label={t('divaClient_fieldInfoText')}
+        ref={refs.setReference}
+        {...getReferenceProps()}
       >
         <InfoIcon />
-      </PopoverButton>
-      <PopoverPanel
-        anchor='top'
-        className={styles['field-info-panel']}
-        role='definition'
-        unmount={false}
-      >
-        <div className={styles['label-wrapper']}>
-          {title && <h4>{t(title)}</h4>}
-          <CloseButton
-            as={Button}
-            variant='icon'
-            size='small'
-            aria-label={t('divaClient_closeText')}
-            tooltipPosition='left'
-          >
-            <CloseIcon />
-          </CloseButton>
-        </div>
-        {body && <p id={ids.details}>{t(body)}</p>}
-      </PopoverPanel>
-    </Popover>
+      </Button>
+      {isOpen && (
+        <FloatingPortal>
+          <FloatingFocusManager context={context}>
+            <div
+              className={styles['field-info-panel']}
+              role='definition'
+              ref={refs.setFloating}
+              style={floatingStyles}
+              {...getFloatingProps()}
+            >
+              <div className={styles['label-wrapper']}>
+                {title && <h4>{t(title)}</h4>}
+                <Button
+                  onClick={() => setIsOpen(false)}
+                  as={Button}
+                  variant='icon'
+                  size='small'
+                  aria-label={t('divaClient_closeText')}
+                  tooltipPosition='left'
+                >
+                  <CloseIcon />
+                </Button>
+              </div>
+              {body && <p id={ids.details}>{t(body)}</p>}
+            </div>
+          </FloatingFocusManager>
+        </FloatingPortal>
+      )}
+    </div>
   );
 };
