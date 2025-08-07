d3704ea1b430a72b2187b89fe60a16196d199cd8
Leo Danielsson <leo.danielsson@ub.uu.se>
2025-08-07 15:15:43 +0200

    Fix accordion focus style

--- app/components/FormGenerator/components/OptionalComponent.tsx
+++ app/components/FormGenerator/components/OptionalComponent.tsx
@@ -78,6 +78,7 @@ export const OptionalComponent = ({
                 aria-label={t('divaClient_addFieldText', {
                   fieldName: t(component.label),
                 })}
+                tooltipPosition='top'
               >
                 <AddCircleIcon /> {t(component.label)}
               </Button>
