f56786f110ee47e6090e5409f10554988a3ed161
Leo Danielsson <leo.danielsson@syson.se>
2025-09-13 20:07:15 +0200

    fix issue where page might load without translations

--- server/i18n.ts
+++ server/i18n.ts
@@ -5,10 +5,11 @@ import { initReactI18next } from 'react-i18next';
 import I18NextHttpBackend from 'i18next-http-backend';
 import { i18nConfig } from '@/i18n/i18nConfig';
 import { createTextDefinition } from '@/data/textDefinition/textDefinition.server';
-import { dependencies } from './depencencies';
+import { getDependencies } from './depencencies';
 
 export const createi18nInstance = async (request: Request) => {
   const i18nInstance = createInstance();
+  const dependencies = await getDependencies();
 
   const languageCookie = await i18nCookie.parse(request.headers.cookie ?? null);
   const locale = languageCookie ?? 'sv';
