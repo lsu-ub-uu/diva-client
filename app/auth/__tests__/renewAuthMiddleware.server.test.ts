64d1bfc897890b67e2bd63d98b9dcfeb410f824b
Leo Danielsson <leo.danielsson@ub.uu.se>
2025-09-29 14:22:19 +0200

    CORA-3133: Fix renewAuth test

--- app/auth/__tests__/renewAuthMiddleware.server.test.ts
+++ app/auth/__tests__/renewAuthMiddleware.server.test.ts
@@ -5,6 +5,7 @@ import { handleRenew } from '../renewAuthMiddleware.server';
 import { type SessionContext } from '../sessionMiddleware.server';
 import { renewAuthToken } from '@/cora/renewAuthToken.server';
 import type { Auth } from '../Auth';
+import { AxiosError } from 'axios';
 
 vi.mock('@/cora/renewAuthToken.server');
 
@@ -40,6 +41,9 @@ describe('renewAuthMiddleware', () => {
     vi.useFakeTimers();
     vi.setSystemTime(new Date('2025-01-01T00:00:00Z'));
 
+    const mockNewAuth = { data: { token: '1234' } } as Auth;
+    vi.mocked(renewAuthToken).mockResolvedValue(mockNewAuth);
+
     const mockSessionContext: SessionContext = {
       auth: {
         data: {
@@ -57,10 +61,74 @@ describe('renewAuthMiddleware', () => {
       t: (translationKey) => translationKey,
     } as i18n;
 
-    handleRenew(mockSessionContext, mockI18n);
-    const mockNewAuth = mock<Auth>();
-    vi.mocked(renewAuthToken).mockReturnValue(Promise.resolve(mockNewAuth));
+    await handleRenew(mockSessionContext, mockI18n);
+
     expect(mockSessionContext.setAuth).toHaveBeenCalledWith(mockNewAuth);
+    expect(mockSessionContext.removeAuth).not.toHaveBeenCalled();
+    expect(mockSessionContext.flashNotification).not.toHaveBeenCalled();
+  });
+
+  it('does not throw an error when failing to renew auth token', async () => {
+    vi.useFakeTimers();
+    vi.setSystemTime(new Date('2025-01-01T00:00:00Z'));
+    const consoleMock = vi.spyOn(console, 'error').mockImplementation(() => {});
+
+    vi.mocked(renewAuthToken).mockRejectedValue(
+      new AxiosError('Unauthorized', '401'),
+    );
+
+    const mockSessionContext: SessionContext = {
+      auth: {
+        data: {
+          validUntil: new Date('2025-01-01T00:00:59Z').getTime().toString(),
+        },
+      } as Auth,
+      setAuth: vi.fn(),
+      removeAuth: vi.fn(),
+      flashNotification: vi.fn(),
+      notification: undefined,
+      destroySession: vi.fn(),
+    };
+
+    const mockI18n = {
+      t: (translationKey) => translationKey,
+    } as i18n;
+
+    await handleRenew(mockSessionContext, mockI18n);
+
+    expect(mockSessionContext.setAuth).not.toHaveBeenCalled();
+    expect(mockSessionContext.removeAuth).not.toHaveBeenCalled();
+    expect(mockSessionContext.flashNotification).not.toHaveBeenCalled();
+    expect(consoleMock).toHaveBeenCalledWith(
+      'Failed to renew auth token',
+      expect.any(AxiosError),
+    );
+  });
+
+  it('does nothing if auth token not expired and not about to expire', async () => {
+    vi.useFakeTimers();
+    vi.setSystemTime(new Date('2025-01-01T00:00:00Z'));
+
+    const mockSessionContext: SessionContext = {
+      auth: {
+        data: {
+          validUntil: new Date('2025-01-01T00:20:00Z').getTime().toString(),
+        },
+      } as Auth,
+      setAuth: vi.fn(),
+      removeAuth: vi.fn(),
+      flashNotification: vi.fn(),
+      notification: undefined,
+      destroySession: vi.fn(),
+    };
+
+    const mockI18n = {
+      t: (translationKey) => translationKey,
+    } as i18n;
+
+    await handleRenew(mockSessionContext, mockI18n);
+
+    expect(mockSessionContext.setAuth).not.toHaveBeenCalled();
     expect(mockSessionContext.removeAuth).not.toHaveBeenCalled();
     expect(mockSessionContext.flashNotification).not.toHaveBeenCalled();
   });
