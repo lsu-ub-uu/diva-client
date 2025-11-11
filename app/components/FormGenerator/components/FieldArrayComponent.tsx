0e355e56447ffc49598419ada3dde12f11735336
Leo Danielsson <leo.danielsson@syson.se>
2025-11-11 07:24:22 +0100

    dnd poc

--- app/components/FormGenerator/components/FieldArrayComponent.tsx
+++ app/components/FormGenerator/components/FieldArrayComponent.tsx
@@ -21,14 +21,34 @@ import { Button } from '@/components/Button/Button';
 import { isComponentSingularAndOptional } from '@/components/FormGenerator/formGeneratorUtils/formGeneratorUtils';
 import type { FormComponentWithData } from '@/components/FormGenerator/types';
 import { AddCircleIcon } from '@/icons';
-import { Fragment, use, useState, type ReactNode } from 'react';
+import { forwardRef, Fragment, use, useState, type ReactNode } from 'react';
 import type { Control } from 'react-hook-form';
 import { useFieldArray } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { createDefaultValuesFromComponent } from '../defaultValues/defaultValues';
 import { FormGeneratorContext } from '../FormGeneratorContext';
 import { ActionButtonGroup } from './ActionButtonGroup';
-
+import {
+  closestCenter,
+  DndContext,
+  DragOverlay,
+  KeyboardSensor,
+  PointerSensor,
+  UniqueIdentifier,
+  useSensor,
+  useSensors,
+  type DragEndEvent,
+  type DragOverEvent,
+  type DragStartEvent,
+} from '@dnd-kit/core';
+import { CSS } from '@dnd-kit/utilities';
+import {
+  SortableContext,
+  sortableKeyboardCoordinates,
+  useSortable,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
+import { posix } from 'path';
 interface FieldArrayComponentProps {
   control?: Control<any>;
   name: string;
@@ -43,7 +63,7 @@ interface FieldArrayComponentProps {
 }
 
 type RepeatingField = {
-  repeatId?: string;
+  repeatId: string;
   id: string;
 };
 
@@ -54,6 +74,15 @@ export const FieldArrayComponent = ({
   renderCallback,
   anchorId,
 }: FieldArrayComponentProps) => {
+  const [activeId, setActiveId] = useState<UniqueIdentifier | null>(null);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
+  );
+  const [overId, setOverId] = useState(null); // Used to conditionally render the dropLine
+  const [dropLinePosition, setDropLinePosition] = useState(''); // To set the preview line where the dragged item will be dropped (top/bottom)
   const { t } = useTranslation();
   const { enhancedFields } = use(FormGeneratorContext);
   const notRemovableEnhancement =
@@ -64,6 +93,8 @@ export const FieldArrayComponent = ({
     control: control,
     name: name,
   });
+
+  console.log({ fields });
 
   const handleAppend = async () => {
     const newComponent = createDefaultValuesFromComponent(component, true);
@@ -79,43 +110,118 @@ export const FieldArrayComponent = ({
     remove(index);
   };
 
+  function handleDragStart(event: DragStartEvent) {
+    const { active } = event;
+    console.log('drag start', event.active.id);
+    setActiveId(active.id);
+  }
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+    console.log('drag end', event.active.id, event.over?.id);
+    setOverId(null);
+    if (active.id !== over?.id) {
+      const oldIndex = fields.findIndex(
+        (field) => field.repeatId === active.id,
+      );
+      const newIndex = fields.findIndex((field) => field.repeatId === over.id);
+
+      move(oldIndex, newIndex);
+    }
+  }
+
+  function handleDragOver(event: DragOverEvent) {
+    const { active, over } = event;
+
+    if (over) {
+      setOverId(over.id);
+
+      const activeIndex = fields.findIndex(
+        (field) => field.repeatId === active.id,
+      );
+      const overIndex = fields.findIndex((field) => field.repeatId === over.id);
+
+      if (activeIndex > overIndex) setDropLinePosition('top');
+      else setDropLinePosition('bottom');
+    }
+  }
+
+  const activeIndex = fields.findIndex((field) => field.repeatId === activeId);
+
   return (
     <>
-      {fields.map((field, index) => {
-        const actionButtonGroup = component.mode === 'input' &&
-          !notRemovableEnhancement && (
-            <ActionButtonGroup
-              entityName={`${t(component.label ?? '')}`}
-              hideMoveButtons={isComponentSingularAndOptional(component)}
-              hideDeleteButton={
-                isComponentSingularAndOptional(component) &&
-                !component.showLabel
-              }
-              moveUpButtonDisabled={index === 0}
-              moveUpButtonAction={() => handleMove(index, index - 1)}
-              moveDownButtonDisabled={index === fields.length - 1}
-              moveDownButtonAction={() => handleMove(index, index + 1)}
-              deleteButtonDisabled={
-                fields.length <= (component.repeat?.repeatMin ?? 1)
-              }
-              deleteButtonAction={() => handleRemove(index)}
-              entityType={component.type}
-            />
-          );
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragStart={handleDragStart}
+        onDragEnd={handleDragEnd}
+        onDragOver={handleDragOver}
+      >
+        <SortableContext items={fields} strategy={verticalListSortingStrategy}>
+          {fields.map((field, index) => {
+            const actionButtonGroup = component.mode === 'input' &&
+              !notRemovableEnhancement && (
+                <ActionButtonGroup
+                  entityName={`${t(component.label ?? '')}`}
+                  hideMoveButtons={isComponentSingularAndOptional(component)}
+                  hideDeleteButton={
+                    isComponentSingularAndOptional(component) &&
+                    !component.showLabel
+                  }
+                  moveUpButtonDisabled={index === 0}
+                  moveUpButtonAction={() => handleMove(index, index - 1)}
+                  moveDownButtonDisabled={index === fields.length - 1}
+                  moveDownButtonAction={() => handleMove(index, index + 1)}
+                  deleteButtonDisabled={
+                    fields.length <= (component.repeat?.repeatMin ?? 1)
+                  }
+                  deleteButtonAction={() => handleRemove(index)}
+                  entityType={component.type}
+                />
+              );
 
-        const repeatId = (field as RepeatingField).repeatId;
+            const repeatId = (field as RepeatingField).repeatId;
 
-        return (
-          <Fragment key={repeatId ?? field.id}>
-            {renderCallback(
-              `${name}[${index}]` as const,
-              actionButtonGroup,
-              index,
-              appended === repeatId,
-            )}
-          </Fragment>
-        );
-      })}
+            return (
+              <SortableItem
+                key={repeatId}
+                id={repeatId}
+                previewLine={overId === repeatId ? dropLinePosition : null}
+                showDragHandle={component.mode === 'input' && fields.length > 1}
+              >
+                {renderCallback(
+                  `${name}[${index}]` as const,
+                  actionButtonGroup,
+                  index,
+                  appended === repeatId,
+                )}
+              </SortableItem>
+            );
+          })}
+        </SortableContext>
+        <DragOverlay>
+          {activeId ? (
+            <div
+              id={activeId}
+              className='form-component-item'
+              data-colspan={12}
+              style={{
+                backgroundColor: 'rgb(255 255 255 /0.5)',
+                transform: 'rotate(2deg)',
+                maxHeight: '300px',
+                overflow: 'hidden',
+              }}
+            >
+              {renderCallback(
+                `${name}[${activeIndex}]` as const,
+                null,
+                activeIndex,
+                false,
+              )}
+            </div>
+          ) : null}
+        </DragOverlay>
+      </DndContext>
 
       {component.mode === 'input' &&
         component.label &&
@@ -143,5 +249,94 @@ export const FieldArrayComponent = ({
           </div>
         )}
     </>
+  );
+};
+
+const Item = forwardRef(({ id, ...props }, ref) => {
+  return (
+    <div {...props} ref={ref}>
+      {id}
+    </div>
+  );
+});
+const SortableItem = ({
+  id,
+  children,
+  showDragHandle = false,
+  previewLine,
+}: {
+  id: string;
+  previewLine?: 'top' | 'bottom' | null;
+  children: ReactNode;
+  showDragHandle?: boolean;
+}) => {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    display: 'flex',
+    gridTemplateColumns: 'auto 1fr',
+    transition,
+    gap: 'var(--gap-s)',
+    ...(previewLine === 'top'
+      ? {
+          borderTop: '2px dashed var(--color-link-selected)',
+          paddingTop: '1rem',
+        }
+      : {}),
+    ...(previewLine === 'bottom'
+      ? {
+          borderBottom: '2px dashed var(--color-link-selected)',
+          paddingBottom: '1rem',
+        }
+      : {}),
+  };
+
+  return (
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      className='form-component-item'
+      data-colspan={12}
+    >
+      <div style={{ flexGrow: 1 }}>{children}</div>
+      {showDragHandle && (
+        <button
+          className='drag-handle'
+          style={{
+            width: '1rem',
+            borderRadius: 'var(--border-radius-s)',
+            border: '1px solid var(--color-thin-border)',
+            background: 'var(--color-background)',
+            cursor: 'grab',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+          {...listeners}
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='1rem'
+            height='1rem'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            stroke-width='2'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+          >
+            <circle cx='9' cy='12' r='1' />
+            <circle cx='9' cy='5' r='1' />
+            <circle cx='9' cy='19' r='1' />
+            <circle cx='15' cy='12' r='1' />
+            <circle cx='15' cy='5' r='1' />
+            <circle cx='15' cy='19' r='1' />
+          </svg>
+        </button>
+      )}
+    </div>
   );
 };
