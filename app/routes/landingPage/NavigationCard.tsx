08d966621fcd335de9abc3c7fe60b83042d19d54
Leo Danielsson <leo.danielsson@ub.uu.se>
2025-12-03 10:30:20 +0100

    CORA-3090 fix landing page nav card styling

--- app/routes/landingPage/NavigationCard.tsx
+++ app/routes/landingPage/NavigationCard.tsx
@@ -2,6 +2,7 @@ import type { LucideIcon } from 'lucide-react';
 import type { ReactNode } from 'react';
 import { Link } from 'react-router';
 import styles from './NavigationCard.module.css';
+import { clsx } from 'clsx';
 
 interface NavigationCardProps {
   to: string;
@@ -24,7 +25,7 @@ export const NavigationCard = ({
     <Link to={to} className={styles['navigation-link']}>
       <div className={styles['navigation-card']}>
         {Icon ? (
-          <Icon className={styles[`card-icon ${iconColor}`]} />
+          <Icon className={clsx(styles[`card-icon`], styles[iconColor])} />
         ) : (
           iconNode
         )}
