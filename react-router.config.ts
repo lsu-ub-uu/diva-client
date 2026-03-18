1bb0a13a386602d18de8bea07e3735b25447cb97
Leo Danielsson <leo.danielsson@ub.uu.se>
2026-03-18 15:38:10 +0100

    remove rr future flags

--- react-router.config.ts
+++ react-router.config.ts
@@ -6,7 +6,5 @@ export default {
   basename: process.env.BASE_PATH ?? undefined,
   future: {
     v8_middleware: true,
-    v8_splitRouteModules: true,
-    v8_viteEnvironmentApi: true,
   },
 } satisfies Config;
