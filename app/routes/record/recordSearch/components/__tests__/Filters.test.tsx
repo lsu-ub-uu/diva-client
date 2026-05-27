0ed2e098afa80875dc60954acb59eebfbb45df5a
Leo Danielsson <leo.danielsson@ub.uu.se>
2026-05-27 14:32:21 +0200

    CORA-3573 Fix search autocomplete filters not applying properly
    Co-authored-by: egiiiiil <egiiiiil@users.noreply.github.com>

---
+++ app/routes/record/recordSearch/components/__tests__/Filters.test.tsx
@@ -0,0 +1,55 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
+import { Filters } from '../Filters';
+import { createRoutesStub } from 'react-router';
+import type { FilterDefinition } from '../../utils/createFilterDefinition.server';
+import type { ActiveFilter } from '../../utils/createActiveFilters.server';
+import { userEvent } from '@testing-library/user-event/dist/cjs/setup/index.js';
+import { act } from 'react';
+
+vi.mock('@/utils/useDebouncedCallback', () => ({
+  useDebouncedCallback: (callback: () => void) => callback,
+}));
+
+describe('Filters', () => {
+  it('renders filters and handles changes', async () => {
+    const user = userEvent.setup();
+    const loaderSpy = vi.fn().mockResolvedValue({});
+    const filters: FilterDefinition[] = [
+      {
+        id: 'filter1',
+        name: 'Filter 1',
+        textId: 'Filter1Text',
+        type: 'text',
+        placeholderTextId: 'Filter1Placeholder',
+        repeat: { repeatMin: 0, repeatMax: 1 },
+      },
+    ];
+    const activeFilters: ActiveFilter[] = [];
+    const query = '';
+    const rows = 10;
+    const RoutesStub = createRoutesStub([
+      {
+        path: '/',
+        loader: loaderSpy,
+        Component: () => (
+          <Filters
+            filters={filters}
+            activeFilters={activeFilters}
+            query={query}
+            rows={rows}
+            onClose={vi.fn()}
+          />
+        ),
+      },
+    ]);
+
+    await act(() => render(<RoutesStub />));
+
+    await user.type(screen.getByRole('textbox', { name: 'Filter1Text' }), 't');
+
+    await waitFor(() => {
+      expect(loaderSpy).toHaveBeenCalled();
+    });
+  });
+});
