344bed3cda997bbf832d565f9e3d0fa3cf53a293
Leo Danielsson <leo.danielsson@ub.uu.se>
2026-06-08 10:38:48 +0200

    CORA-3313 Refactor mime type handling

---
+++ app/routes/resourceRoutes/utils/__tests__/mimeTypeToFileExtension.test.ts
@@ -0,0 +1,97 @@
+import { describe, expect, it } from 'vitest';
+import { mimeTypeToFileExtension } from '../mimeTypeToFileExtension';
+describe('mimeTypeToFileExtension', () => {
+  it.each([
+    ['audio/aac', 'aac'],
+    ['application/x-abiword', 'abw'],
+    ['image/apng', 'apng'],
+    ['application/x-freearc', 'arc'],
+    ['image/avif', 'avif'],
+    ['video/x-msvideo', 'avi'],
+    ['application/vnd.amazon.ebook', 'azw'],
+    ['application/octet-stream', 'bin'],
+    ['image/bmp', 'bmp'],
+    ['application/x-bzip', 'bz'],
+    ['application/x-bzip2', 'bz2'],
+    ['application/x-cdf', 'cda'],
+    ['application/x-csh', 'csh'],
+    ['text/css', 'css'],
+    ['text/csv', 'csv'],
+    ['application/msword', 'doc'],
+    [
+      'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+      'docx',
+    ],
+    ['application/vnd.ms-fontobject', 'eot'],
+    ['application/epub+zip', 'epub'],
+    ['application/gzi', 'gz'],
+    ['image/gif', 'gif'],
+    ['text/html', 'html'],
+    ['image/vnd.microsoft.icon', 'ico'],
+    ['text/calendar', 'ics'],
+    ['application/java-archive', 'jar'],
+    ['image/jpeg', 'jpg'],
+    ['text/javascript', 'js'],
+    ['application/json', 'json'],
+    ['application/ld+json', 'jsonld'],
+    ['text/markdown', 'md'],
+    ['audio/midi', 'midi'],
+    ['audio/x-midi', 'midi'],
+    ['audio/mpeg', 'mp3'],
+    ['video/mp4', 'mp4'],
+    ['video/mpeg', 'mpeg'],
+    ['application/vnd.apple.installer+xml', 'mpkg'],
+    ['application/vnd.oasis.opendocument.presentation', 'odp'],
+    ['application/vnd.oasis.opendocument.spreadsheet', 'ods'],
+    ['application/vnd.oasis.opendocument.text', 'odt'],
+    ['audio/ogg', 'oga'],
+    ['video/ogg', 'ogv'],
+    ['application/ogg', 'ogx'],
+    ['font/otf', 'otf'],
+    ['image/png', 'png'],
+    ['application/pdf', 'pdf'],
+    ['application/x-httpd-php', 'php'],
+    ['application/vnd.ms-powerpoint', 'ppt'],
+    [
+      'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+      'pptx',
+    ],
+    ['application/vnd.rar', 'rar'],
+    ['application/rtf', 'rtf'],
+    ['application/x-sh', 'sh'],
+    ['image/svg+xml', 'svg'],
+    ['application/x-tar', 'tar'],
+    ['image/tiff', 'tiff'],
+    ['video/mp2t', 'ts'],
+    ['font/ttf', 'ttf'],
+    ['text/plain', 'txt'],
+    ['application/vnd.visio', 'vsd'],
+    ['audio/wav', 'wav'],
+    ['audio/webm', 'weba'],
+    ['video/webm', 'webm'],
+    ['application/manifest+json', 'webmanifest'],
+    ['image/webp', 'webp'],
+    ['font/woff', 'woff'],
+    ['font/woff2', 'woff2'],
+    ['application/xhtml+xml', 'xhtml'],
+    ['application/vnd.ms-excel', 'xls'],
+    [
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      'xlsx',
+    ],
+    ['application/xml', 'xml'],
+    ['application/vnd.mozilla.xul+xml', 'xul'],
+    ['application/zip', 'zip'],
+    ['video/3gpp', '3gp'],
+    ['application/x-7z-compressed', '7z'],
+  ])('Given MIME %s, returns %s', (mimeType, expectedExtension) => {
+    const extension = mimeTypeToFileExtension(mimeType);
+    expect(extension).toBe(expectedExtension);
+  });
+
+  it('handles unknown MIME types by returning the subtype as extension', () => {
+    const mimeType = 'application/xyz';
+    const extension = mimeTypeToFileExtension(mimeType);
+    expect(extension).toBe('xyz');
+  });
+});
