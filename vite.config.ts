bc5b443b8fd9c527411b969416ec8d3f7098df16
Leo Danielsson <leo.danielsson@ub.uu.se>
2026-07-02 10:55:59 +0200

    Switch to chromium for browser tests

--- vite.config.ts
+++ vite.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
             provider: playwright(),
             enabled: true,
             headless: true,
-            instances: [{ browser: 'firefox' }],
+            instances: [{ browser: 'chromium' }],
           },
         },
       },
