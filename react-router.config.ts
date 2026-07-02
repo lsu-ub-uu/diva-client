9601284b3425403944f77700b11d54866318387c
Leo Danielsson <leo.danielsson@ub.uu.se>
2026-07-02 10:05:08 +0200

    Upgrade to React Router 8.1.0  (#780)
    * upgrade to react router 8.0.1
    
    * Fix module duplication issues
    
    * Update breadcrumbs after RR8
    
    * fix data -> loaderData
    
    * migrate AttributesSelect test to browser mode
    
    * bump react router to 8.1.0
    
    * Install react router agent skill
    
    ---------
    
    Co-authored-by: Leo Danielsson <leo.danielsson@syson.se>

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,11 +4,4 @@ export default {
   ssr: true,
   buildDirectory: 'dist',
   basename: process.env.BASE_PATH ?? undefined,
-  future: {
-    v8_middleware: true,
-    v8_viteEnvironmentApi: true,
-    v8_splitRouteModules: true,
-    v8_passThroughRequests: true,
-    v8_trailingSlashAwareDataRequests: true,
-  },
 } satisfies Config;
