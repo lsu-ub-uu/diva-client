b1c76e7e8a538abe4998491a429b1ec56a5be17b
Leo Danielsson <leo.danielsson@ub.uu.se>
2026-07-03 15:15:13 +0200

    Restore changes

--- app/routes/resourceRoutes/binary.ts
+++ app/routes/resourceRoutes/binary.ts
@@ -2,7 +2,7 @@ import { sessionContext } from '@/auth/sessionMiddleware.server';
 import { coraBinaryUrl } from '@/cora/helper.server';
 import { log, logError } from '@/logging/logger.server';
 import type { Route } from './+types/binary';
-import { transformCoraBinaryResponse } from './utils/binaryUtils';
+import { transformCoraBinaryResponse } from './utils/binaryUtils.server';
 
 export const loader = async ({ context, params }: Route.LoaderArgs) => {
   const { auth } = context.get(sessionContext);
