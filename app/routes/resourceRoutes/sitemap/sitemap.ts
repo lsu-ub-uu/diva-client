6f0cedcbee19b9cd86ea2c9ef979e79445ea6ff2
Leo Danielsson <leo.danielsson@ub.uu.se>
2026-08-19 15:00:40 +0200

    add debug logging for sitemap url
    Co-authored-by: egiiiiil <egiiiiil@users.noreply.github.com>

--- app/routes/resourceRoutes/sitemap/sitemap.ts
+++ app/routes/resourceRoutes/sitemap/sitemap.ts
@@ -3,9 +3,15 @@ import { getMemberFromHostname } from '@/utils/getMemberFromHostname';
 import { getDependencies } from 'server/dependencies/depencencies';
 import { generateSitemapXml } from './generateSitemapXml.server';
 import type { Route } from './+types/sitemap';
+import { log } from '@/logging/logger.server';
 
 export const loader = async ({ request, url }: Route.LoaderArgs) => {
   const dependencies = await getDependencies();
+
+  log.info('request.url: ' + request.url);
+  log.info('url.href: ' + url.href);
+  log.info('url: ' + url);
+
   const member = getMemberFromHostname(request, dependencies);
   const navigation = await getNavigation(
     dependencies,
